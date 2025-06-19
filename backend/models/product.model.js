6bc1256010f713edd747d5002210d06b05656072
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-06-19 08:13:22 -0700

    second  commit

---
+++ backend/models/product.model.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    comment: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: { type: String, required: true, trim: true },
+    desc: { type: String, required: true },
+    price: { type: Number, required: true },
+    percentOff: { type: Number, default: 0 },
+    checkStock: { type: Boolean, default: true },
+    units: { type: Number, default: 0 },
+    sku: { type: String, required: true, unique: true },
+    color: [{ type: String }],
+    size: [{ type: String }],
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Woman's Fashion",
+        "Men's Fashion",
+        "Electronics",
+        "Home & Lifestyle",
+        "Medicine",
+        "Sports & Outdoor",
+        "Baby's & Toys",
+        "Groceries & Pets",
+        "Health & Beauty",
+      ],
+    },
+    brand: { type: String, required: true },
+    reviews: [reviewSchema],
+    rating: { type: Number, default: 0 },
+    numReviews: { type: Number, default: 0 },
+    tags: [{ type: String }],
+    soldUnits: { type: Number, default: 0 },
+    isFeatured: { type: Boolean, default: false },
+    images: [
+      {
+        url: {
+          type: String,
+          required: true,
+        },
+        altText: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
---
+++ backend/models/user.model.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    address:String,
+    refreshToken:String
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+userSchema.methods.comparePassword = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
