ffb9404d02af969de9f4c114366814ddeb72cd20
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-07-05 02:11:04 -0700

    add vercel file .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
