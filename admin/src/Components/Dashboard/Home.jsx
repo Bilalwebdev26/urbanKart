650a287b8404191827db0460271f37ebe50fc4cf
bilalwebdev26 <f2022266833@umt.edu.pk>
2025-10-06 17:22:31 +0500

    Header

--- admin/src/Components/Dashboard/Home.jsx
+++ admin/src/Components/Dashboard/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../Common/Header'
 
 const Home = () => {
   return (
-    <div className=''>Home</div>
+    <div className="">
+      <Header/>
+    </div>
   )
 }
 
